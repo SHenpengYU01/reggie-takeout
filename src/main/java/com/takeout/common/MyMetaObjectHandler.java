14c319647a954285579dfcfb9055aebac772220c
mythlogos <s163754732@163.com>
2026-03-19 21:08:45 +0800

    fix bug: 1.解决新增菜品/员工/套餐显示“失败了”问题：修改MyMetaObjectHandler.java，但逻辑似乎仍不完善 2.解决“套餐管理”页面打不开问题

--- src/main/java/com/takeout/common/MyMetaObjectHandler.java
+++ src/main/java/com/takeout/common/MyMetaObjectHandler.java
@@ -1,11 +1,10 @@
 package com.takeout.common;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.takeout.utils.BaseContext;
 import org.apache.ibatis.reflection.MetaObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 
 /**
@@ -15,10 +14,6 @@ import java.time.LocalDateTime;
  */
 @Component
 public class MyMetaObjectHandler implements MetaObjectHandler {
-
-    /*退而求其次选择了注入Request对象*/
-    @Autowired
-    HttpServletRequest httpServletRequest;
 
     /**
      * @param metaObject 插入时自动填充
@@ -31,19 +26,17 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
         //填充 更新的时间
         metaObject.setValue("updateTime", LocalDateTime.now());
         //BaseContext工具类获取当前登陆人员信息
-       //填充创建人信息
-        metaObject.setValue("createUser", httpServletRequest.getSession().getAttribute("employee"));
-        //填充更新人信息
-        metaObject.setValue("updateUser", httpServletRequest.getSession().getAttribute("employee"));
-        /*
-        这里有Bug，就是封装好的BaseContext通过ThreadLocal获取不了对象，虽然都是一个线程的
-        但就是获取不到，所以这里先写死了，后面慢慢再改吧
-        * */
-        /*//填充创建人信息
-        metaObject.setValue("createUser", 1L);
-        //填充更新人信息
-        metaObject.setValue("updateUser", 1L);*/
-
+        //填充创建人信息
+        Long currentId = BaseContext.getCurrentId();
+        if (currentId != null) {
+            metaObject.setValue("createUser", currentId);
+            //填充更新人信息
+            metaObject.setValue("updateUser", currentId);
+        } else {
+            // 如果无法获取当前用户ID，使用默认值避免数据库错误
+            metaObject.setValue("createUser", 0L);
+            metaObject.setValue("updateUser", 0L);
+        }
     }
 
     /**
@@ -55,11 +48,11 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
         //更新 更新的时间
         metaObject.setValue("updateTime", LocalDateTime.now());
         //更新更新人员
-        /*
-        这里有Bug，就是封装好的BaseContext通过ThreadLocal获取不了对象，所以这里先写死了，后面慢慢再改吧
-        metaObject.setValue("updateUser", 1L);
-        * */
-
-        metaObject.setValue("updateUser", httpServletRequest.getSession().getAttribute("employee"));
+        Long currentId = BaseContext.getCurrentId();
+        if (currentId != null) {
+            metaObject.setValue("updateUser", currentId);
+        } else {
+            metaObject.setValue("updateUser", 0L);
+        }
     }
 }
